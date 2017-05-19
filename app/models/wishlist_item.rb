799433faf05921505c630a9d901ee2dda66711c6
Steven Fleming <sfleming@optoro.com>
2017-05-19 16:56:35 -0400

    changed the WishlistItem lookups to use not inactive

--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -3,9 +3,9 @@ class WishlistItem < ApplicationRecord
   belongs_to :item
 
   def self.build_index
-    items = WishlistItem.where(priority: 'active').map(&:item)
+    items = WishlistItem.where("priority != 'inactive'").map(&:item)
     items.uniq.map do |item|
-      qtys = WishlistItem.where(priority: 'active', item_id: item.id).map(&:quantity)
+      qtys = WishlistItem.where("priority != 'inactive' AND item_id = ?", item.id).map(&:quantity)
       item.quantity = qtys.sum
       item
     end
