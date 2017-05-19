bba8c5e758916b4167f9200ee945efe98d71b976
Steven Fleming <sfleming@optoro.com>
2017-05-19 16:10:55 -0400

    got all the AR assocations fixed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  has_many :wishlist_items
+  has_many :wishlists, through: :wishlist_items
 end
--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -1,4 +1,15 @@
 class WishlistItem < ApplicationRecord
   belongs_to :wishlist
-  has_one :item
+  belongs_to :item
+
+  def self.build_index
+    items = WishlistItems.where(priority: 'active').map(&:item)
+    items.map do |item|
+      qtys = WishlistItem.where(priority: 'active', item_id: item.id).map(&:quantity)
+      item.quantity = qtys.sum
+    end
+  end
+
+  def index_params
+  end
 end
