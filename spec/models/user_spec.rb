3a367c067eeb26d4652cd27ee3f4fc636469969c
Evan Light <elight@heroku.com>
2017-05-20 10:24:48 -0400

    Negative case test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,10 @@ describe User do
   let!(:wishlist) { Wishlist.create!(name: "test") }
   let!(:user)     { User.create!(email: "foo@bar.com") }
 
+  it "a user, not associated with a wishlist and not an admin, cannot manage a wishlist" do
+    expect(user.can_manage?(wishlist)).to be_falsey
+  end
+
   describe "who is a site manager" do
     before { user.site_managers.create!(wishlist: wishlist) }
 
