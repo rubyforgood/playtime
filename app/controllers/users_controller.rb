6e130b57c806632cb87815407204c6cd30be46e4
Steven Fleming <sfleming@optoro.com>
2017-05-20 16:24:14 -0400

    the users index is only available to the admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin
 
   # GET /users
   # GET /users.json
