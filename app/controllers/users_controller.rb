5c34c1704caaa1fb0d2a83e26f80cfb60c4bbefb
Lee Sharma <lee@leesharma.com>
2017-06-30 22:47:55 -0400

    Refactor routes for Rails conventions
    Look at all this deleted stuff!
    
    This commit focused on refactoring the routes to conform more to Rails
    conventions so that they can be more compact and readable. Bonus points:
    all routes are now RESTful. Major changes:
    
      - Remove unused Item views
      - Remove ItemsController
      - Add AmazonSearchController to control the search instead
      - Move CSV exports to #index function (no need for a separate action!)
        They can now be found at `/users.csv` and `/pledges.csv`.
    
    Tradeoffs:
    
    The search path helpers are now more verbose. I think it's worth the
    tradeoff (bonus: we won't have to change if we add searching for
    wishlists, etc.), but let me know if you disagree!
    
    The search paths have also changed to be `wishlists/:id/amazon_search`.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    respond_to do |format|
+      format.html { @users = User.all }
+      format.csv  { export_csv }
+    end
   end
 
   def show
@@ -24,10 +27,6 @@ class UsersController < ApplicationController
     redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
-  def export_csv
-    send_data User.generate_csv, filename: "user_data#{Time.now.to_i}.csv"
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -37,5 +36,9 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :admin, :site_manager)
+    end
+
+    def export_csv
+      send_data(User.generate_csv, filename: "user_data#{Time.now.to_i}.csv")
     end
 end
