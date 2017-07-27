527f50236c317cacc362281269608f53e24f1ba2
Micah Bales <micahbales@gmail.com>
2017-07-27 16:38:44 -0400

    remove unused code

--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -28,14 +28,4 @@ class WishlistItem < ApplicationRecord
                        numericality: { greater_than_or_equal_to: 0 }
 
   scope :active, -> { where.not(priority: :inactive) }
-
-  def self.build_index
-    items = WishlistItem.where("priority != 'inactive'").map(&:item)
-    items.uniq.map do |item|
-      qtys = WishlistItem.where("priority != 'inactive' AND item_id = ?", item.id).map(&:quantity)
-      item.quantity = qtys.sum
-      item
-    end
-  end
-
 end
