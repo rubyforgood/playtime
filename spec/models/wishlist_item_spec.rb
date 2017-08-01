01c2fc70ef4cc91ce514a4f778d3f28851bbe57f
Micah Bales <micahbales@gmail.com>
2017-08-01 15:33:14 -0400

    Resolves #71

--- spec/models/wishlist_item_spec.rb
+++ spec/models/wishlist_item_spec.rb
@@ -37,20 +37,4 @@ describe WishlistItem do
     subject { build(:wishlist_item, quantity: -10) }
     it { should_not be_valid }
   end
-
-  describe ".active" do
-    before {
-      create(:wishlist_item, priority: :high)
-      create(:wishlist_item, priority: :low)
-      create(:wishlist_item, priority: :inactive)
-    }
-
-    it "should not return inactive wishlist items" do
-      expect(WishlistItem.active.pluck(:priority)).not_to include "inactive"
-    end
-
-    it "should include all active wishlist items" do
-      expect(WishlistItem.active.count).to eq 2
-    end
-  end
 end
